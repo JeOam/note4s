9ef9af80dc5c24b25ed20c6468b8371580ae7fb9
JeOam <jeoamlx@icloud.com>
2016-02-03 09:30:47 +0800

    update

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,5 +2,6 @@
 
 from .git_note import create_git_repo, note_history, \
     git_tag_list, git_commit_list
+from .markdown import markdown
 
 __author__ = 'JeOam'
--- app/views/note_detail.py
+++ app/views/note_detail.py
@@ -1,12 +1,18 @@
 # coding=utf-8
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
+from app.models import Note
+from app.services import markdown
 
 __author__ = 'JeOam'
-
 
 def note_detail(request, uuid_str):
     """
     返回笔记详情页
     """
-    return render_to_response("app/note_detail.html")
+    note = get_object_or_404(Note, pk=uuid_str)
+    context = {
+        "note_content": markdown(note.content)
+    }
+    return render_to_response("app/note_detail.html",
+                              context=context)
