74fe2e7d96f9e62e29f79c779faf735dc0dc6a43
JeOam <lxjamsterdam@gmail.com>
2021-01-13 20:38:26 +0800

    更新设置

--- note4s/settings.py
+++ note4s/settings.py
@@ -12,9 +12,9 @@ REDIS_HOST = os.environ.get("REDIS_HOST", "127.0.0.1")
 REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 REDIS_DB = os.environ.get("REDIS_DB", 1)
 
-GITHUB_ID = os.environ.get("GITHUB_ID", "6bc8273bcb63a84de3ba")
-GITHUB_SECRET = os.environ.get("GITHUB_ID", "426b6e5feecf23c58187b99cc285438259285ba2")
-MAILGUN_KEY = os.environ.get("MAILGUN_KEY", "key-7b67fe0ce64c61fc44792de3d43fa0ac")
+GITHUB_ID = os.environ.get("GITHUB_ID", "")
+GITHUB_SECRET = os.environ.get("GITHUB_ID", "")
+MAILGUN_KEY = os.environ.get("MAILGUN_KEY", "")
 
 PG_URL = os.environ.get("PG_URL", "postgres+psycopg2://note4s:note4s@127.0.0.1:5432/note4s?sslmode=disable")
 GIT_DIR = os.environ.get("GIT_DIR", "GIT_DIR/")
